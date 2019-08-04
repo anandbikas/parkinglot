1455b371ef78a6464483110429a26106680596aa
Bikas Anand <anand.iitr.mca@gmail.com>
2019-08-04 23:31:33 +0530

    Add author signature.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ParkingLot System
 Parking Lot
+
+
+####Author:
+- Bikas Anand<br>
+Sr. Software Engineer (SDE 3)<br>
+Git: https://github.com/anandbikas<br>
+LinkedIn: https://linkedin.com/in/anand-iitr-mca<br>
+Alma Mater: Indian Institute of Technology, Roorkee.<br>
+ 
--- src/main/java/com/anand/parkinglot/parking/Colour.java
+++ src/main/java/com/anand/parkinglot/parking/Colour.java
@@ -1,3 +1,10 @@
+/**
+ * @Author:     Bikas Anand,
+ *              Sr. Software Engineer (SDE 3).
+ *              Git: https://github.com/anandbikas
+ *              LinkedIn: https://linkedin.com/in/anand-iitr-mca/
+ *              Alma Mater: Indian Institute of Technology, Roorkee.
+ */
 package com.anand.parkinglot.parking;
 
 import java.util.LinkedHashMap;
